b62edb815d43dca2feafab53706a29d38cc3354e
Sweezy28 <99082633@mydavinci.nl>
2024-09-02 11:53:28 +0100

    Les 2 opdrachten
    Dit zijn les 2 opdrachten

---
+++ bc_l2_doemee.py
@@ -0,0 +1,13 @@
+#Krijgt dit dezelfde uitkomst? 
+print( 5*2 -3+4/2 )
+print( 5*2 - 3+4 / 2 )
+
+#En hoe zit het met deze?
+#print( (5*2) - (3+4) /2 )
+#print( ((5*2) -(3+4)) / 2 )
+
+#Bekijk de volgende code, wat denk je dat eruit komt?
+#print( (431 / 100) * 100 )
+
+#Wat gebeurt hier nu weer:
+#print( 3 * "hallo" ) 
